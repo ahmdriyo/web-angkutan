af7e3a349af0edbd6dbbbe375ef34267f18b4373
ahmdriyo <ahmdriyo.tbn.com>
2024-07-08 07:18:02 +0800

    cange color loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -9,7 +9,7 @@ const Loading = () => {
         width="80"
         ariaLabel="color-ring-loading"
         wrapperClass="color-ring-wrapper"
-        colors={['#9299fa', '#7077df', '#4e54a4', '#8185c5', '#b2b5e0']}
+        colors={['#c0942f', '#aa9565', '#f4b420', '#71f420', '#449213']}
       />
     </div>
   );
